8a46ba4b2052c360e21d7d30a4ee722438448dd0
kritika909 <126762803+kritika909@users.noreply.github.com>
2024-12-25 22:54:24 +0530

    Update views.py

--- book/views.py
+++ book/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Book, Review, ReadingHistory
 from .recommendations import get_collaborative_rec, get_content_based_rec
-# Create your views here.
+
 
 @login_required
 def book(request):
